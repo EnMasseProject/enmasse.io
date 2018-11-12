4cf1ede48c1b09976c3b63c6b3922c18a2199cf2
Ulf Lilleengen <lulf@redhat.com>
2018-11-12 12:35:27 +0100

    Update doc versions

--- build.rb
+++ build.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require 'fileutils'
 
-VERSIONS=["master", "0.24.0", "0.23.0", "0.22.0"]
+VERSIONS=["master", "0.24.1", "0.23.2", "0.22.0"]
 
 LATEST_VERSION=VERSIONS[1] 
 OLD_VERSIONS=VERSIONS.drop(2)
