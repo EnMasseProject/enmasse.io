9a43193aac5fbed90581c71a37d7767b8a36fede
Ulf Lilleengen <lulf@redhat.com>
2019-08-27 10:23:52 +0200

    Update versions

--- build.rb
+++ build.rb
@@ -2,7 +2,7 @@
 require 'fileutils'
 
 PLATFORMS=["kubernetes", "openshift"]
-VERSIONS=["master", "0.29.0", "0.28.2", "0.27.2", "0.26.5"]
+VERSIONS=["master", "0.29.1", "0.28.3", "0.27.2", "0.26.5"]
 
 LATEST_VERSION=VERSIONS[1] 
 OLD_VERSIONS=VERSIONS.drop(2)
